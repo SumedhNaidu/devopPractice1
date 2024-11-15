061d423b250206dc2ef702afb028a4f9e70f0fd9
SumedhNaidu <sumedhnaidu.kn@gmail.com>
2024-11-15 15:40:16 +0530

    First Practice Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[2][];
+            arr[0] = new int [] { 1,2,3,4 };
+            arr[1] = new int[] { 5, 6, 7 };
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.Write("{ ");
+                for(int j = 0; j < arr[i].Length;j++)
+                {
+                    Console.Write(arr[i][j]+ " ");
+                }
+                Console.WriteLine(" }");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
